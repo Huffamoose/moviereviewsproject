6d558911bef1b01324ef380365262396a795613c
Huffamoose <wedelkirt@gmail.com>
2023-12-17 17:43:07 -0500

    added update a review

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-from .models import Movie
+from django.shortcuts import get_object_or_404, redirect
+from .models import Movie, Review
+from .forms import ReviewForm
 
 
 def home(request):
@@ -24,4 +25,45 @@ def signup(request):
 
 def detail(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
-    return render(request, "detail.html", {"movie": movie})
+    reviews = Review.objects.filter(movie=movie)
+    return render(request, "detail.html", {"movie": movie, "reviews": reviews})
+
+
+def createreview(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    if request.method == "GET":
+        return render(
+            request, "createreview.html", {"form": ReviewForm(), "movie": movie}
+        )
+    else:
+        try:
+            form = ReviewForm(request.POST)
+            newReview = form.save(commit=False)
+            newReview.user = request.user
+            newReview.movie = movie
+            newReview.save()
+            return redirect("detail", newReview.movie_id)
+        except ValueError:
+            return render(
+                request,
+                "createreview.html",
+                {"form": ReviewForm(), "error": "Bad data passed in"},
+            )
+
+
+def updatereview(request, review_id):
+    review = get_object_or_404(Review, pk=review_id, user=request.user)
+    if request.method == "GET":
+        form = ReviewForm(instance=review)
+        return render(request, "updatereview.html", {"review": review, "form": form})
+    else:
+        try:
+            form = ReviewForm(request.POST, instance=review)
+            form.save()
+            return redirect("detail", review.movie_id)
+        except ValueError:
+            return render(
+                request,
+                "updatereview.html",
+                {"review": review, "form": form, "error": "Bad data in form"},
+            )
