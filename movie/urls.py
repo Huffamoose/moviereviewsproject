86ba80f75f4df7ea5a8f055f9b2dcbb33774645f
Huffamoose <wedelkirt@gmail.com>
2023-12-17 13:22:50 -0500

    implemented login & logout

--- movie/urls.py
+++ movie/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [        
-    path('<int:movie_id>/', views.detail, name='detail'),
+urlpatterns = [
+    path("<int:movie_id>/", views.detail, name="detail"),
 ]
--- news/models.py
+++ news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class News(models.Model):
     headline = models.CharField(max_length=200)
     body = models.TextField()
@@ -7,4 +8,3 @@ class News(models.Model):
 
     def __str__(self):
         return self.headline
-    
